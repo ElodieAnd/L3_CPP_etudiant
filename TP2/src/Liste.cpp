13498eb3400daf1d672f609ed88af19682a800fb
ElodieAnd <elodieandrieux62100@gmail.com>
2017-02-08 10:44:34 +0100

    Q2

---
+++ TP2/src/Liste.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+struct Noeud {
+  int _valeur;
+  Noeud * _suivant;
+};
+  
+class Liste {
+  Noeud * _tete;
+
+  Liste() {
+
+  }
+
+  void ajouterDevant(int val){
+
+  }
+
+  const int getTaille(){
+
+  }
+
+  const int getElement(int indice){
+
+  }
+  
+	       
+};
